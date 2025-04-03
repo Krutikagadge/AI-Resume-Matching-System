3da5774917006ffd54795081feb069b0c1074a6e
Krutika Gadge <krutikargadge29@gmail.com>
2025-04-03 20:21:22 +0530

    Add files via upload

---
+++ Resume_Analyze/Courses.py
@@ -0,0 +1,179 @@
+ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+             ['Machine Learning A-Z by Udemy','https://www.udemy.com/course/machinelearning/'],
+             ['Machine Learning by Andrew NG','https://www.coursera.org/learn/machine-learning'],
+             ['Data Scientist Master Program of Simplilearn (IBM)','https://www.simplilearn.com/big-data-and-analytics/senior-data-scientist-masters-program-training'],
+             ['Data Science Foundations: Fundamentals by LinkedIn','https://www.linkedin.com/learning/data-science-foundations-fundamentals-5'],
+             ['Data Scientist with Python','https://www.datacamp.com/tracks/data-scientist-with-python'],
+             ['Programming for Data Science with Python','https://www.udacity.com/course/programming-for-data-science-nanodegree--nd104'],
+             ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
+             ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
+             ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230']]
+
+web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+              ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+              ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+              ['ReactJS Project Development Training','https://www.dotnettricks.com/training/masters-program/reactjs-certification-training'],
+              ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+              ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+              ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+              ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+              ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+              ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']]
+
+android_course = [['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
+                  ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
+                  ['Associate Android Developer Certification','https://grow.google/androiddev/#?modal_active=none'],
+                  ['Become an Android Kotlin Developer by Udacity','https://www.udacity.com/course/android-kotlin-developer-nanodegree--nd940'],
+                  ['Android Basics by Google','https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803'],
+                  ['The Complete Android Developer Course','https://www.udemy.com/course/complete-android-n-developer-course/'],
+                  ['Building an Android App with Architecture Components','https://www.linkedin.com/learning/building-an-android-app-with-architecture-components'],
+                  ['Android App Development Masterclass using Kotlin','https://www.udemy.com/course/android-oreo-kotlin-app-masterclass/'],
+                  ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
+                  ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64']]
+
+ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
+              ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
+              ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
+              ['iOS App Development with Swift Specialization','https://www.coursera.org/specializations/app-development'],
+              ['Mobile App Development with Swift','https://www.edx.org/professional-certificate/curtinx-mobile-app-development-with-swift'],
+              ['Swift Course by LinkedIn','https://www.linkedin.com/learning/subscription/topics/swift-2'],
+              ['Objective-C Crash Course for Swift Developers','https://www.udemy.com/course/objectivec/'],
+              ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
+              ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
+              ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ']]
+uiux_course = [['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
+               ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['UX & Web Design Master Course: Strategy, Design, Development','https://www.udemy.com/course/ux-web-design-master-course-strategy-design-development/'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['DESIGN RULES: Principles + Practices for Great UI Design','https://www.udemy.com/course/design-rules/'],
+               ['Become a UX Designer by Udacity','https://www.udacity.com/course/ux-designer-nanodegree--nd578'],
+               ['Adobe XD Tutorial: User Experience Design Course [Free]','https://youtu.be/68w2VwalD5w'],
+               ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
+               ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
+
+resume_videos = ['https://youtu.be/3agP4x8LYFM','https://youtu.be/fS_t3yS8v5s',
+                 'https://youtu.be/aArb68OBFPg','https://youtu.be/h-NuvOeWWh0',
+                 'https://youtu.be/BdQniERyw8I','https://youtu.be/Tt08KmFfIYQ',
+                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/bhwEsfXS6y8']
+
+interview_videos = ['https://youtu.be/Tt08KmFfIYQ','https://youtu.be/KukmClH1KoA',
+                    'https://youtu.be/7_aAicmPB3A','https://youtu.be/1mHjMNZZvFo',
+                    'https://youtu.be/WfdtKbAJOmE','https://youtu.be/wFbU185CvDU'
+                    'https://youtu.be/wFbU185CvDU','https://youtu.be/TZ3C_syg9Ow']
+
+
+
+
+
+
+hr_course = [
+    ['HR Management and Analytics by Coursera', 'https://www.coursera.org/specializations/human-resource-management-analytics'],
+    ['Strategic HR Management by edX', 'https://www.edx.org/course/strategic-human-resources-management'],
+    ['HR Fundamentals by Udemy', 'https://www.udemy.com/course/hr-fundamentals/']
+]
+
+arts_course = [
+    ['Social Media Marketing by HubSpot Academy [Free]', 'https://academy.hubspot.com/courses/social-media'],
+    ['Graphic Design Basics by Canva [Free]', 'https://www.canva.com/designschool/courses/graphic-design-basics/'],
+    ['The Art of Visual Storytelling by LinkedIn Learning', 'https://www.linkedin.com/learning/the-art-of-visual-storytelling']
+]
+
+
+
+mechanical_course = [
+    ['Mechanical Engineering and Electrical Engineering Explained', 'https://www.udemy.com/course/mechanical-and-electrical-engineering/'],
+    ['CAD and Digital Manufacturing by Coursera', 'https://www.coursera.org/specializations/cad-digital-manufacturing']
+]
+
+sales_course = [
+    ['Sales Training by HubSpot Academy [Free]', 'https://academy.hubspot.com/courses/sales-training'],
+    ['Sales and Marketing Masterclass by Udemy', 'https://www.udemy.com/course/sales-and-marketing-masterclass/']
+]
+
+health_fitness_course = [
+    ['Nutrition and Health by Coursera', 'https://www.coursera.org/learn/stanford-introduction-to-food-and-health'],
+    ['Personal Trainer Certification by Udemy', 'https://www.udemy.com/course/personal-trainer-certification/']
+]
+
+civil_course = [
+    ['Civil Engineering Basics by Udemy', 'https://www.udemy.com/course/civil-engineering-basics/'],
+    ['Structural Engineering by Coursera', 'https://www.coursera.org/specializations/structural-engineering']
+]
+
+java_dev_course = [
+    ['Java Programming by Udemy', 'https://www.udemy.com/course/java-programming-tutorial-for-beginners/'],
+    ['Java Spring Boot by Coursera', 'https://www.coursera.org/specializations/java-spring-boot']
+]
+
+business_analyst_course = [
+    ['Business Analytics by Harvard', 'https://online.hbs.edu/courses/business-analytics/'],
+    ['Business Intelligence by Coursera', 'https://www.coursera.org/specializations/business-intelligence']
+]
+
+sap_dev_course = [
+    ['SAP ERP Essential Training by LinkedIn Learning', 'https://www.linkedin.com/learning/sap-erp-essential-training']
+]
+
+automation_testing_course = [
+    ['Selenium WebDriver with Java', 'https://www.udemy.com/course/selenium-webdriver-java/'],
+    ['Automated Software Testing by Coursera', 'https://www.coursera.org/learn/automated-software-testing']
+]
+
+electrical_eng_course = [
+    ['Introduction to Electrical Engineering by Coursera', 'https://www.coursera.org/learn/electrical-engineering'],
+    ['Power Systems Engineering by edX', 'https://www.edx.org/course/power-systems-engineering']
+]
+
+operations_manager_course = [
+    ['Operations Management by Coursera', 'https://www.coursera.org/specializations/operations-management'],
+    ['Supply Chain and Logistics by Udemy', 'https://www.udemy.com/course/supply-chain-logistics-management']
+]
+
+python_dev_course = [
+    ['Python for Everybody by Coursera', 'https://www.coursera.org/specializations/python'],
+    ['Python Data Structures by Udacity', 'https://www.udacity.com/course/data-structures-and-algorithms-in-python--ud513']
+]
+
+devops_course = [
+    ['DevOps Fundamentals by Udemy', 'https://www.udemy.com/course/devops-fundamentals/'],
+    ['Docker and Kubernetes by Coursera', 'https://www.coursera.org/specializations/docker-kubernetes']
+]
+
+network_security_course = [
+    ['Cyber Security by Stanford University', 'https://online.stanford.edu/courses/cs155-computer-and-network-security'],
+    ['Network Security by Coursera', 'https://www.coursera.org/specializations/network-security']
+]
+
+pmo_course = [
+    ['Project Management for Beginners by Coursera', 'https://www.coursera.org/learn/project-management'],
+    ['PMO Fundamentals by Udemy', 'https://www.udemy.com/course/project-management-office-pmo-fundamentals']
+]
+
+database_course = [
+    ['Database Design and Management by Udemy', 'https://www.udemy.com/course/database-design/'],
+    ['SQL for Data Science by Coursera', 'https://www.coursera.org/learn/sql-for-data-science']
+]
+
+hadoop_course = [
+    ['Big Data with Hadoop by Coursera', 'https://www.coursera.org/learn/big-data-hadoop'],
+    ['Hadoop Fundamentals by Udemy', 'https://www.udemy.com/course/hadoop-fundamentals']
+]
+
+etl_dev_course = [
+    ['ETL and Data Warehousing by Udemy', 'https://www.udemy.com/course/etl-and-data-warehousing/']
+]
+
+dotnet_dev_course = [
+    ['ASP.NET Core Fundamentals by Udemy', 'https://www.udemy.com/course/aspnet-core-fundamentals/']
+]
+
+blockchain_course = [
+    ['Blockchain Basics by Coursera', 'https://www.coursera.org/learn/blockchain-basics'],
+    ['Ethereum and Solidity by Udemy', 'https://www.udemy.com/course/ethereum-and-solidity']
+]
+
+testing_course = [
+    ['Software Testing Masterclass by Udemy', 'https://www.udemy.com/course/software-testing-masterclass'],
+    ['Manual Testing for Beginners by Udemy', 'https://www.udemy.com/course/manual-testing-for-beginners']
+]
